1a99d5227aa1066fdbc0daf557793c0fd73043e8
DrDoom-art <abhinavupy69@gmail.com>
2025-07-25 13:00:53 +0530

    Setup Strapi database config with .env:

--- config/database.ts
+++ config/database.ts
@@ -1,53 +1,25 @@
-import path from 'path';
-
 export default ({ env }) => {
   const client = env('DATABASE_CLIENT', 'postgres');
 
   const connections = {
-    mysql: {
-      connection: {
-        host: env('DATABASE_HOST', 'dpg-d21i3nadbo4c73e6p8ag-a'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi_db_cla8'),
-        user: env('DATABASE_USERNAME', 'strapi_db_cla8_user'),
-        password: env('DATABASE_PASSWORD', 'eoZtD68ZD4QGezxhFbkDYN37etDWOlLI'),
-        ssl: env.bool('DATABASE_SSL', true) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'dpg-d21i3nadbo4c73e6p8ag-a'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi_db_cla8'),
-        user: env('DATABASE_USERNAME', 'strapi_db_cla8_user'),
-        password: env('DATABASE_PASSWORD', 'eoZtD68ZD4QGezxhFbkDYN37etDWOlLI'),
-        ssl: env.bool('DATABASE_SSL', true) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL') && {
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED'),
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-    sqlite: {
-      connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      pool: {
+        min: env.int('DATABASE_POOL_MIN', 2),
+        max: env.int('DATABASE_POOL_MAX', 10),
       },
-      useNullAsDefault: true,
     },
+    // mysql and sqlite omitted for brevity
   };
 
   return {
