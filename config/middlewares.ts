990056336a69a21f070dc1e1847c7d06602ed19e
DrDoom-art <abhinavupy69@gmail.com>
2025-07-27 12:08:29 +0530

    updated middleware config

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -6,11 +6,8 @@ export default [
     config: {
       enabled: true,
       origin: [
-        'http://localhost:5500',
-        'http://127.0.0.1:5500',
-        'http://localhost:1337'
+        'https://lustrous-paprenjak-255715.netlify.app'
       ],
-      headers: '*',
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       credentials: true
     }
